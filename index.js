b5634402d659fc8108028fc6558fc35205517b18
Richard Hoffman <coverslide@gmail.com>
2013-03-04 13:21:28 -0800

    Changed response type to ArrayBuffer, handle inflater responses that are not data

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function InflateStream(stream){
   var inflater = new Inflater()
   var _this = this
   stream.on('data', function(data){
-    _this.emit('data', inflater.append(new Uint8Array(data)))
+    var decompressedData = inflater.append(new Uint8Array(data))
+    if(decompressedData.buffer)
+      _this.emit('data', decompressedData.buffer)
+    else
+      _this.emit('error', new Error('Received response ' + decompressedData + ' from inflater'))
   })
 
   stream.on('end', function(data){
